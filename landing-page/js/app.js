1220d90439017da1009b05f9b922f74fdf61931f
Lauren Maurer <lauren@openmindplatform.org>
2021-09-30 19:50:32 -0400

    changed styling to travel theme

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -21,10 +21,10 @@ for (i=0; i<navSections.length; i++){
 const getSectionHeader = (div) => {
     return div.firstElementChild.textContent;
 }
-// returns the id of the section which contains each div in .landing__container class
-const getSectionId = (div) => {
-    return div.parentElement.id;
-}
+// // returns the id of the section which contains each div in .landing__container class
+// const getSectionId = (div) => {
+//     return div.parentElement.id;
+// }
 
 // for each section to be added to navBar:
 // // get the header & id to populate the li content
@@ -33,14 +33,14 @@ const getSectionId = (div) => {
 const navBuilder = () => {
     for (let i=0; i<navSections.length; i++){
         let sectionHeader = getSectionHeader(navSections[i]);
-        let sectionId = getSectionId(navSections[i]);
         let navItem = document.createElement('li');
-        navItem.innerHTML = `<a href="#${sectionId}">${sectionHeader}</a>`;
-        navItem.id = `nav-link-${i+1}`
+        // need to use id to implement scroll-to
+        navItem.innerHTML = `<a id="nav-link-${i+1}">${sectionHeader}</a>`;
         navItem.className = 'nav-link'
         navBar.appendChild(navItem);
     }
 }
+//href="#${sectionId}"
 
 // get dimensions in viewport for each section
 const getRectSections = () => {
@@ -55,8 +55,8 @@ const getRectSections = () => {
 // returns the section currently in the viewport
 const inViewport = () => {
     let rectSections = getRectSections();
-    for (i=0; i<rectSections.length; i++){
-        if (rectSections[i].top >= 0 && rectSections[i].left >= 150){
+    for (let i=0; i<rectSections.length; i++){
+        if (rectSections[i].top >= 0 && rectSections[i].height >= 300){
             i++;
             return `Section ${i}`;
         }
@@ -72,30 +72,14 @@ document.addEventListener('DOMContentLoaded', navBuilder, false);
 
 // // execute function every 500ms
 setInterval( () => {
+    // only execute the function when scrolling has been detected
     if ( hasScrolled ) {
-        // reset boolean 
+        // reset boolean so scroll-check will remain accurate
         hasScrolled = false;
+
         // add "active" classes to section & its nav item when that section is in the viewport
+        // NOT WORKING FOR NARROW WINDOWS - is it supposed to?
 
-        //more concise draft — doesn't work yet
-        // for (let h=1; h<(sectionContainers.length+1); h++){
-        //     while (inViewport() === `Section [h]`){
-        //         for (let i=0; i<sectionContainers.length; i++){
-        //             console.log(`i=${i}`);
-        //             if (i === (h-1)) {
-        //                 sectionContainers[i].classList.add('active-section');
-        //                 navItems[i].classList.add('active');
-                        
-        //             } else {
-        //                 sectionContainers[i].classList.remove('active-section');
-        //                 navItems[i].classList.remove('active');
-        //             }
-        //         }
-        //         console.log(`h=${h}`);
-        //     }
-        // }
-
-        // NOT WORKING FOR MOBILE :(
         if (inViewport() === 'Section 1'){
             for (let i=0; i<sectionContainers.length; i++){
                 if (i === 0) {
@@ -136,18 +120,20 @@ setInterval( () => {
                     navItems[i].classList.remove('active');
                 }
             }
+        } else if (inViewport() === 'Section 5'){
+            for (let i=0; i<sectionContainers.length; i++){
+                if (i === 4) {
+                    sectionContainers[i].classList.add('active-section');
+                    navItems[i].classList.add('active');
+                } else {
+                    sectionContainers[i].classList.remove('active-section');
+                    navItems[i].classList.remove('active');
+                }
+            }
         }
     }
 }, 500);
 
-setInterval( () => {
-    if ( !hasScrolled ) {
-        pageHeader.setAttribute('style','background-color: #000');
-    }
-    if ( hasScrolled ) {
-        pageHeader.setAttribute('style','background-color: hsla(0, 0%, 0%, 0.850)');
-    }
-}, 100);
 
 /* - Events - */
 
@@ -155,13 +141,96 @@ setInterval( () => {
 let hasScrolled = false;
 window.onscroll = () => hasScrolled = true;
 
+// make header slightly transparent while user is scrolling
+setInterval( () => {
+    if ( !hasScrolled ) {
+        pageHeader.setAttribute('style','background-color: rgba(79, 83, 100, 0.15)');
+    }
+    if ( hasScrolled ) {
+        pageHeader.setAttribute('style','background-color: rgba(73, 75, 85, 0.3)');
+        // navBar.setAttribute('style', 'display: none');
+    }
+}, 100);
+
 // Scroll to section on link click
 
 // // sets click listener for each item in navBar
 // // prevents default jump; implements scroll instead
+
+const scrollToSection = (index) => {
+    navSections[index].scrollIntoView(true, {behavior: 'smooth', block: 'top', inline: 'nearest'});
+}
+
+// THIS ISN'T WORKING!
 for (let i=0; i<navItems.length; i++){
-    navItems[i].addEventListener('click', function (event) {
-        event.preventDefault();
-        navSections[i].scrollIntoView(true, {behavior: 'smooth', block: 'top', inline: 'nearest'});
-    });
+    navItems[i].addEventListener('click', scrollToSection(i), false);
+}
+
+// this doesn't work, probably because it can't add a click event to that element
+// navItems[0].addEventListener('click', scrollToSection(i), false);
+
+
+    // event.preventDefault();
+ //more concise draft — doesn't work yet
+        // for (let h=1; h<(sectionContainers.length+1); h++){
+        //     while (inViewport() === `Section ${h}`){
+        //         for (let i=0; i<sectionContainers.length; i++){
+        //             console.log(`i=${i}`);
+        //             if (i === (h-1)) {
+        //                 sectionContainers[i].classList.add('active-section');
+        //                 navItems[i].classList.add('active');
+                        
+        //             } else {
+        //                 sectionContainers[i].classList.remove('active-section');
+        //                 navItems[i].classList.remove('active');
+        //             }
+        //         }
+        //         console.log(`h=${h}`);
+        //     }
+        // }
+
+
+        //try setting this up as a while loop?
+        // seems to crash the page because it keeps running as long as it's true
+        // for (let h=0; h<sectionContainers.length; h++){
+        //     while (inViewport() === h) {
+        //         for (let i=0; i<sectionContainers.length; i++){
+        //             if (i = h) {
+        //                 sectionContainers[i].classList.add('active-section');
+        //                 navItems[i].classList.add('active');
+        //             } else {
+        //                 // doesn't work because it's removing classes from the same sections it just added to ?
+        //                 sectionContainers[i].classList.remove('active-section');
+        //                 navItems[i].classList.remove('active');
+        //             }
+        //         }
+        //     }
+        // }
+
+
+// needs to be adapted 
+// Get the modal
+const modalGallery = document.getElementById("modal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("open");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
