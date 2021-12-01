2363f7ed25fd45d99480d64b7cd55f4664fb45f4
Lauren Maurer <lauren@openmindplatform.org>
2021-11-30 19:02:43 -0500

    modularize subfunctions of formHandler

--- NLP_App/src/client/js/formHandler.js
+++ NLP_App/src/client/js/formHandler.js
@@ -1,5 +1,5 @@
-import { postForm } from "./dataPoster";
 import { validateLang } from './langValidator';
+import { configSentiments} from "./sentimentConfigurator";
 
 async function handleSubmit(event) {
     event.preventDefault();
@@ -8,12 +8,8 @@ async function handleSubmit(event) {
     const endpoint = 'https://api.meaningcloud.com/sentiment-2.1';
     const MeaningCloud_API_Key = '979efb0428313b854a9125ee2da216c7';
     
-
     // check what text was put into the form field
     let formText = document.getElementById('input-text').value;
-
-    // not working, not necessary
-    // postForm('/text-input',formText);
 
     const isValidated = await validateLang(formText);
 
@@ -26,38 +22,27 @@ async function handleSubmit(event) {
             // Transform into JSON
             const response = await request.json()
             .then(response => {
-                document.getElementById('results-title').innerHTML= `Sentiments Detected:`;
-
-                document.getElementById('agreement').innerHTML = `<strong>Agreement:</strong> ${response.agreement.toLowerCase()}`;
-                const agreementEmoji = (response.agreement.toLowerCase() === 'agreement' ? '&#128077;' : '&#128078;');
-                document.getElementById('agreement-emoji').innerHTML = agreementEmoji;
-                
-
-                document.getElementById('subjectivity').innerHTML = `<strong>Subjectivity:</strong> ${response.subjectivity.toLowerCase()}`;
-                const subjectivityEmoji = (response.subjectivity.toLowerCase() === 'objective' ? '&#129488;' : '&#129300;');
-                document.getElementById('subjectivity-emoji').innerHTML = subjectivityEmoji;
-
-                document.getElementById('confidence').innerHTML = `<strong>Confidence:</strong> ${response.confidence}%`;
-                const confidenceEmoji = (response.confidence >= 70 ? '&#128526;' : '&#129320;');
-                document.getElementById('confidence-emoji').innerHTML = confidenceEmoji;
-
-                document.getElementById('irony').innerHTML = `<strong>Irony:</strong> ${response.irony.toLowerCase()}`;
-                const ironyEmoji = (response.irony.toLowerCase() === 'ironic' ? '&#128521;' : '&#128528;');
-                document.getElementById('irony-emoji').innerHTML = ironyEmoji;
-
-
-                document.getElementById('sentiments').style.visibility = 'visible';
-                document.getElementById('results').style.visibility = 'visible';
+                // retrieve data from API & populate DOM content
+                configSentiments(response);
             })
             .then( () => {
-                document.getElementById('sentiments').classList.add('animate__animated', 'animate__fadeInDown');
+                // display retrieved sentiment data in results div
+                document.getElementById('sentiments').style.visibility = 'visible';
+                
+                // add animations
+                document.getElementById('sentiments').classList.add = 'animate__animated animate__fadeInDown';
+                const sentiments = document.getElementsByClassName('sentiment-grid');
+                for (let i=0; i < sentiments.length; i++) {
+                    sentiments[i].classList.add('animate__animated','animate__zoomIn','animate__slow');
+                }
+
+                // show div containing sentiments
+                document.getElementById('results').style.visibility = 'visible';
             })
         } catch(error) {
             console.log(error);
         }
-    } else {
-
-    }
+    } 
 }
 
 export { handleSubmit }
