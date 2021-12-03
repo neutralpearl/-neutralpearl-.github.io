1641e91f76ee39989fcfe678631f96822fb5a74a
Lauren Maurer <lauren@openmindplatform.org>
2021-12-03 09:25:27 -0500

    fix dotenv path error

--- NLP_App/src/server/index.js
+++ NLP_App/src/server/index.js
@@ -1,12 +1,14 @@
 const path = require('path');
-const process = require('process'); // unless localhost:8080 is viewed in Chrome incognito mode, browser displays "ReferenceError: process is not defined" when running functions that use variables defined as properties of process.env.
+// const process = require('process'); // unless localhost:8080 is viewed in Chrome incognito mode, browser displays "ReferenceError: process is not defined" when running functions that use variables defined as properties of process.env.
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv'); // supposed to enable use of environmental variables
-dotenv.config({path: path.join(__dirname, '.env')})
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+// console.log(path.resolve(__dirname, "../.env"));
 
 const API_KEY = process.env.API_KEY;
-console.log(API_KEY); // prints "undefined"; variable not getting pulled from .env & passed correctly into GET response
+console.log(API_KEY); 
+console.log(JSON.stringify({key: API_KEY})); // debugging
 
 const app = express();
 
@@ -29,8 +31,8 @@ app.listen(8081, () => {
 })
 
 // callback function for GET route (allows client-side JS to retrieve API key from server)
-const getKey = (req, res) => {
-    res.status(200).send(JSON.stringify({key: API_KEY}));
-}
+// const getKey = 
 // GET route — NOT WORKING!
-app.get('/get-key', getKey);
+app.get('/get-key', (req, res) => {
+    res.status(200).send(JSON.stringify({key: API_KEY}));
+});
