f6ac52cc04b60e5a4bbbb4921d1d12701d0638b4
Lauren Maurer <lauren@openmindplatform.org>
2021-12-03 00:05:03 -0500

    configured webpack-dev-server; restructured HTML

--- TravelApp/webpack.dev.js
+++ TravelApp/webpack.dev.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -24,6 +26,18 @@ module.exports = {
         ]
     },
     plugins: [
-        
+        new HtmlWebPackPlugin({
+            template: "./src/client/html/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
     ]
 }
